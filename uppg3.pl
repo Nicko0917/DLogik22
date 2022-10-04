64b0c85526dbb159db85a27099a2ea278c9de1c5
Gustav Nordström <gnords@kth.se>
2022-10-04 21:45:49 +0200

    Uppgift 3

--- uppg3.pl
+++ uppg3.pl
@@ -1,5 +1,13 @@
-subsetx([], _, []).
-subsetx([E|Tail], L, [E|NTail]):-
-  subsetx(Tail, _, NTail), length([E|NTail], L).
-subsetx([_|Tail], L1, NTail):-
-  subsetx(Tail, L1, NTail).
+/* First part will remove the head of the first list
+until the heads of the lists are the same. It will then go to sequence */
+partstring([], _, []).
+partstring([_|Tail], L, List) :-
+    partstring(Tail, L, List).
+partstring([H|Tail1], L, [H|Tail2]) :-
+    sequence(Tail1, Tail2), length([H|Tail2], L).
+
+/* Will return false if heads are not equal.
+Removes heads of the lists until the sub-sequence is an empty list.  */
+sequence(_, []).
+sequence([H|Tail1], [H|Tail2]) :-
+    sequence(Tail1, Tail2).
